ecced3f6911d81f43eab3c2da6ca31a9c2903ad3
Valentino Strebel <tino_schweizer@hotmail.com>
2025-10-26 16:18:08 +0100

    update

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(("authentication.urls", "authentication"), namespace="authentication")),
     path("api/", include(("video.urls", "video"), namespace="video")),
+    path("django-rq/", include("django_rq.urls")),
 ]
